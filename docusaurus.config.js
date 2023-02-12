7150dc2f1fe8cccb56c24c7b4ab1fbabd6a0ac87
Nick Siderakis <nixsid@gmail.com>
2023-02-12 11:23:03 -0500

    .

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "opensourceblueprint", // Usually your GitHub org/user name.
   projectName: "opensourceblueprint.github.io", // Usually your repo name.
-
+  deploymentBranch: "gh-pages",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   trailingSlash: false,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link className="button button--secondary button--lg" to="/intro">
-            - 5min ⏱️
+            Intro - 5 min ⏱️
           </Link>
         </div>
       </div>
